1567ec11b9918a17d9f02bc0ee1ed6ba1be89d53
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-28 20:31:56 +0700

    untuk menyediakan instance homeviewmodel dan entryviewmodel menggunakan repositori dari aplikasi siswa

---
+++ app/src/main/java/com/example/myroom1_035/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.myroom1_035.viewmodel.provider
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroom1_035.repository.AplikasiSiswa
+import com.example.myroom1_035.viewmodel.EntryViewModel
+import com.example.myroom1_035.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
