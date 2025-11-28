5f6b58e9a7ef7376f58c02369970a9e7237208f4
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-28 20:17:36 +0700

    route untuk halaman home pada aplikasi

---
+++ app/src/main/java/com/example/myroom1_035/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.myroom1_035.view.route
+
+import com.example.myroom1_035.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
