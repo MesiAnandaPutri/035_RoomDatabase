504758135dc43ac84d3fff222146074f761c48e5
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-28 20:01:56 +0700

    Interface sebagai kontrak untuk menyediakan depedency repository siswa

---
+++ app/src/main/java/com/example/myroom1_035/repository/ContainerApp.kt
@@ -0,0 +1,9 @@
+package com.example.myroom1_035.repository
+
+import android.app.Application
+import android.content.Context
+import com.example.myroom1_035.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
