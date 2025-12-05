8d659a0f88d8d71a44bc706a41a53965fcffca94
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-12-05 15:23:36 +0700

    Konfigurasi rute detail siswa dan argumen navigasi

---
+++ app/src/main/java/com/example/myroom1_035/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.myroom1_035.view.route
+
+import com.example.myroom1_035.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
