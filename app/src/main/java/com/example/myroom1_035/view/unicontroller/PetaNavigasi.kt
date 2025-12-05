ca64575fc8b25f22138a54ade7948d6a88f27965
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-12-05 15:45:01 +0700

    Routing ke halaman detail siswa dengan argumen ID

--- app/src/main/java/com/example/myroom1_035/view/unicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroom1_035/view/unicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myroom1_035.view.DetailSiswaScreen
 import com.example.myroom1_035.view.EntrySiswaScreen
 import com.example.myroom1_035.view.HomeScreen
+import com.example.myroom1_035.view.route.DestinasiDetailSiswa
+import com.example.myroom1_035.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.myroom1_035.view.route.DestinasiEntry
 import com.example.myroom1_035.view.route.DestinasiHome
 
@@ -39,6 +44,15 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(
                 navigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg){
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = {navController.navigateUp()}
             )
         }
     }
